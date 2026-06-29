245a5e08bbc35b021ac973a1944189fbcbb5828f
Gabriel Fiori <140767553+GabFiori@users.noreply.github.com>
2026-06-29 16:41:49 -0300

    Update dfs.py

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -2,14 +2,44 @@ from puzzle.base_search import BaseSearch
 from puzzle.state import State
 from puzzle.result import SearchResult
 
-DEFAULT_DEPTH_LIMIT = 50
-
-
 class DFS(BaseSearch):
-
-    def __init__(self, depth_limit: int = DEFAULT_DEPTH_LIMIT):
-        self.depth_limit = depth_limit
-
     def search(self, initial: State) -> SearchResult:
-        # TODO: implemente a DFS aqui
-        raise NotImplementedError("Implemente o método search na classe DFS.")
+        if initial.is_goal:
+            return SearchResult(solution=initial, nodes_generated=1)
+
+        frontier = [initial]
+        explored = set()
+        frontier_set = {initial}
+        nodes_expanded = 0
+        nodes_generated = 1
+        max_frontier_size = 1
+
+        while frontier:
+            max_frontier_size = max(max_frontier_size, len(frontier))
+            current = frontier.pop()
+            frontier_set.remove(current)
+            
+            explored.add(current)
+            nodes_expanded += 1
+
+            if current.is_goal:
+                return SearchResult(
+                    solution=current,
+                    nodes_expanded=nodes_expanded,
+                    nodes_generated=nodes_generated,
+                    max_frontier_size=max_frontier_size,
+                    depth=current.cost
+                )
+
+            for neighbor in reversed(current.neighbors()):
+                if neighbor not in explored and neighbor not in frontier_set:
+                    frontier.append(neighbor)
+                    frontier_set.add(neighbor)
+                    nodes_generated += 1
+
+        return SearchResult(
+            solution=None,
+            nodes_expanded=nodes_expanded,
+            nodes_generated=nodes_generated,
+            max_frontier_size=max_frontier_size
+        )
